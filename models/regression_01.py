ad01cdf5e001cfdc357290ffa183aa47cd4bba30
sophia-helena-paula <sophia-helena-paula>
2024-09-09 18:40:38 +0000

    feat: model import libs
    Initial commit for stating the model and it's imports

---
+++ models/regression_01.py
@@ -0,0 +1,34 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # 01 - Import libs
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import OneHotEncoder
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml import Pipeline
+from pyspark.ml import PipelineModel
+from pyspark.ml.evaluation import RegressionEvaluator
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
